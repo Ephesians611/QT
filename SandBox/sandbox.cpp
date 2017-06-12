33d2c92c3df3d2ca04261b56d6605c8196660733
Sloans <Johnny.Sloans@thermon.com>
2017-06-12 17:41:31 -0500

    running

--- SandBox/sandbox.cpp
+++ SandBox/sandbox.cpp
@@ -12,6 +12,7 @@
 #include <QDial>
 #include <QGroupBox>
 #include <QPushButton>
+#include <QRadioButton>
 
 int main(int argc, char *argv[])
 {
@@ -32,50 +33,64 @@ int main(int argc, char *argv[])
                         btnLoadTrace->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);	// to avoid stretching the button
 
         QGroupBox *grpSimulationInputControls = new QGroupBox("Simulation Input");
-            QHBoxLayout *loutSimulationInput = new QHBoxLayout;
+            QHBoxLayout *hloutSimulationInput = new QHBoxLayout;
+            QVBoxLayout *vloutSimulationInput = new QVBoxLayout;
                 QComboBox *cboReplacementPolicy = new QComboBox();
                     cboReplacementPolicy->addItem("LRU");
                     cboReplacementPolicy->addItem("LFU");
                     cboReplacementPolicy->addItem("Random");
-                QComboBox *cboCacheType = new QComboBox();
-                    cboCacheType->addItem("Instruction");
-                    cboCacheType->addItem("Data");
-                QSpinBox *spnSets = new QSpinBox;
-                    spnSets->setRange(0, 100);
+                QGroupBox *grpCacheType = new QGroupBox();
+                    QRadioButton *rdoInstruction = new QRadioButton("Instruction");
+                    QRadioButton *rdoData = new QRadioButton("Data");
+                    rdoInstruction->setChecked(true);
+            QGridLayout *grdloutSimulationInput = new QGridLayout;
+                QSlider *hsSets = new QSlider(Qt::Horizontal);
+                    hsSets->setRange(1, 16);
+                QLCDNumber *lcdNumSets = new QLCDNumber();\
+                    lcdNumSets->setSegmentStyle(QLCDNumber::Flat);
                 QSpinBox *spnWays = new QSpinBox;
-                    spnWays->setRange(0, 100);
-                QSpinBox *spnBytesPerBlock = new QSpinBox;
-                    spnBytesPerBlock->setRange(0, 100);
+                    spnWays->setRange(1, 16);
+                QLCDNumber *lcdNumWays = new QLCDNumber();
+                    lcdNumWays->setSegmentStyle(QLCDNumber::Flat);
+                QDial *dialBytesPerBlock = new QDial();
+                    dialBytesPerBlock->setRange(4, 1024);
+                QLCDNumber *lcdNumBytesPerBlock = new QLCDNumber();
+                    lcdNumBytesPerBlock->setSegmentStyle(QLCDNumber::Flat);
 
         QGroupBox *grpSimulationControls = new QGroupBox("Simulation Control");
-            QGridLayout *loutSimulationControl = new QGridLayout;
+            QGridLayout *grdloutSimulationControl = new QGridLayout;
                 QPushButton *btnRun = new QPushButton("Start");
                         btnRun->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);	// to avoid stretching the button
-                QPushButton *btnStep = new QPushButton("Go");
-                        btnStep->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);	// to avoid stretching the button
+                QPushButton *btnContinue = new QPushButton("Continue");
+                        btnContinue->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);	// to avoid stretching the button
                 QPushButton *btnRunNumSteps = new QPushButton("Step S");
                         btnRunNumSteps->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);	// to avoid stretching the button
+                QSpinBox *spnNumSteps = new QSpinBox;
+                    spnNumSteps->setRange(1, 16);
                 QPushButton *btnRunUntil = new QPushButton("Break B");
                         btnRunUntil->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);	// to avoid stretching the button
+                QSpinBox *spnBreakAt = new QSpinBox;
+                    spnBreakAt->setRange(1, 16);
 
         QGroupBox *grpSimulationOutputControls = new QGroupBox("Simulation Output");
-            QGridLayout *loutSimulationOutput = new QGridLayout;
-                QLCDNumber *LCDStation = new QLCDNumber();
-                    LCDStation->setDigitCount(4);
-                    LCDStation->setMode(QLCDNumber::Dec);
-                    LCDStation->display(535);
-                    LCDStation->setSegmentStyle(QLCDNumber::Flat);
-
+            QGridLayout *grdloutSimulationOutput = new QGridLayout;
+                QLCDNumber *lcdMemAccessInstances = new QLCDNumber();\
+                    lcdMemAccessInstances->setSegmentStyle(QLCDNumber::Flat);
+                QLCDNumber *lcdCompMissRatio = new QLCDNumber();
+                    lcdCompMissRatio->setSegmentStyle(QLCDNumber::Flat);
+                QLCDNumber *lcdHitRatio = new QLCDNumber();
+                    lcdHitRatio->setSegmentStyle(QLCDNumber::Flat);
+                QLCDNumber *lcdMissRatio = new QLCDNumber();
+                    lcdMissRatio->setSegmentStyle(QLCDNumber::Flat);
 
         QPushButton *quit = new QPushButton("Quit");
                 quit->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);	// to avoid stretching the button
 
     // Widget Connections
-    QObject::connect(HSlider,   SIGNAL(valueChanged(int)),  LCDStation,     SLOT(display(int)));
-    QObject::connect(dial,      SIGNAL(valueChanged(int)),  LCDVolume,      SLOT(display(int)));
-    QObject::connect(VSlider,   SIGNAL(valueChanged(int)),  LCDBass,        SLOT(display(int)));
-    QObject::connect(spinBox,   SIGNAL(valueChanged(int)),  LCDTreble,      SLOT(display(int)));
-    QObject::connect(quit,      SIGNAL(clicked()),          qApp,           SLOT(quit()));  // apply quit function to quit button
+    QObject::connect(hsSets,            SIGNAL(valueChanged(int)),  lcdNumSets,         SLOT(display(int)));
+    QObject::connect(spnWays,           SIGNAL(valueChanged(int)),  lcdNumWays,         SLOT(display(int)));
+    QObject::connect(dialBytesPerBlock, SIGNAL(valueChanged(int)),  lcdNumBytesPerBlock,SLOT(display(int)));
+    QObject::connect(quit,              SIGNAL(clicked()),          qApp,               SLOT(quit()));  // apply quit function to quit button
 
     // Widget Layouts
     //
@@ -84,22 +99,35 @@ int main(int argc, char *argv[])
     grpSimulationTraceFileInput->setLayout(loutLoadTrace);
 
     // Child Simulation Input Controls Layout
-    layout1->addWidget(comboBox);
-    layout1->addWidget(LCDStation);
-    layout2->addLayout(layout1);
-    layout2->addWidget(HSlider);
-    grpSimulationInputControls->setLayout(layout2);
+    vloutSimulationInput->addWidget(cboReplacementPolicy);
+    vloutSimulationInput->addWidget(grpCacheType);
+
+    grdloutSimulationInput->addWidget(hsSets,               0, 0, Qt::AlignBottom);
+    grdloutSimulationInput->addWidget(lcdNumSets,           1, 0, Qt::AlignBottom);
+    grdloutSimulationInput->addWidget(spnWays,              0, 1, Qt::AlignBottom);
+    grdloutSimulationInput->addWidget(lcdNumWays,           1, 1, Qt::AlignBottom);
+    grdloutSimulationInput->addWidget(dialBytesPerBlock,    0, 2, Qt::AlignCenter);
+    grdloutSimulationInput->addWidget(lcdNumBytesPerBlock,  2, 2, Qt::AlignTop);
+
+    hloutSimulationInput->addLayout(vloutSimulationInput);
+    hloutSimulationInput->addLayout(grdloutSimulationInput);
+    grpSimulationInputControls->setLayout(hloutSimulationInput);
 
     // Child Simulation Controls Layout
-    voiceLayout->addWidget(dial, 0, 0, Qt::AlignCenter);
-    voiceLayout->addWidget(LCDVolume, 1, 0);
-    voiceLayout->addWidget(VSlider, 0, 1, Qt::AlignCenter);
-    voiceLayout->addWidget(LCDBass, 1, 1);
-    voiceLayout->addWidget(spinBox, 0, 2, Qt::AlignCenter);
-    voiceLayout->addWidget(LCDTreble, 1, 2);
-    grpSimulationControls->setLayout(voiceLayout);
+    grdloutSimulationControl->addWidget(btnRun,         0, 0, Qt::AlignCenter);
+    grdloutSimulationControl->addWidget(btnContinue,    1, 0);
+    grdloutSimulationControl->addWidget(btnRunNumSteps, 0, 1, Qt::AlignCenter);
+    grdloutSimulationControl->addWidget(spnNumSteps,    1, 1);
+    grdloutSimulationControl->addWidget(btnRunUntil,    0, 2);
+    grdloutSimulationControl->addWidget(spnBreakAt,     1, 2);
+    grpSimulationControls->setLayout(grdloutSimulationControl);
 
     // Child Simulation Output Controls Layout
+    grdloutSimulationOutput->addWidget(lcdMemAccessInstances,   0, 0, Qt::AlignCenter);
+    grdloutSimulationOutput->addWidget(lcdCompMissRatio,        1, 0, Qt::AlignCenter);
+    grdloutSimulationOutput->addWidget(lcdHitRatio,             0, 1, Qt::AlignCenter);
+    grdloutSimulationOutput->addWidget(lcdMissRatio,            1, 1, Qt::AlignCenter);
+    grpSimulationOutputControls->setLayout(grdloutSimulationOutput);
 
     // Organize Child Layouts
     loutMain->addWidget(grpSimulationTraceFileInput);
