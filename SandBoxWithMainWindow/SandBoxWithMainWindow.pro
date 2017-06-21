afeb295d035e0350736cae8cf3e54ade3fae313d
Sloans <Johnny.Sloans@thermon.com>
2017-06-21 10:42:07 -0500

    assignmentI2

---
+++ SandBoxWithMainWindow/SandBoxWithMainWindow.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2017-06-21T10:03:24
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = SandBoxWithMainWindow
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ SandBoxWithMainWindow/mainwindow.h
@@ -0,0 +1,102 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QApplication>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QtGui>
+#include <QSlider>
+#include <QSpinBox>
+#include <QComboBox>
+#include <QLCDNumber>
+#include <QLabel>
+#include <QDial>
+#include <QGroupBox>
+#include <QPushButton>
+#include <QRadioButton>
+#include <QTextEdit>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void open();
+    void help();
+
+private:
+    Ui::MainWindow *ui;
+
+    void createActions();
+    void createMenus();
+    void createControls();
+
+    QMenu *fileMenu;
+    QMenu *helpMenu;
+
+    QAction *openAct;
+    QAction *exitAct;
+    QAction *helpAct;
+
+    QTextEdit *editor;
+
+    QWidget  *dockCentralWidget;
+
+    QVBoxLayout *loutMain;
+
+        QGroupBox *grpSimulationTraceFileInput;
+            QHBoxLayout *loutLoadTrace;
+                QPushButton *btnLoadTrace;
+
+        QGroupBox *grpSimulationInputControls;
+            QHBoxLayout *hloutSimulationInput;
+            QVBoxLayout *vloutSimulationInput;
+                QLabel *lblReplacementPolicy;
+                QComboBox *cboReplacementPolicy;
+                QLabel *lblCacheType;
+                QRadioButton *rdoInstruction;
+                QRadioButton *rdoData;
+            QGridLayout *grdloutSimulationInput;
+                QLabel *lblSets;
+                QSlider *hsSets;
+                QLCDNumber *lcdNumSets;\
+                QLabel *lblWays;
+                QSpinBox *spnWays;
+                QLCDNumber *lcdNumWays;
+                QLabel *lblBytesPerBlock;
+                QDial *dialBytesPerBlock;
+                QLCDNumber *lcdNumBytesPerBlock;
+
+        QGroupBox *grpSimulationControls;
+            QGridLayout *grdloutSimulationControl;
+                QPushButton *btnRun;
+                QPushButton *btnContinue;
+                QPushButton *btnRunNumSteps;
+                QSpinBox *spnNumSteps;
+                QPushButton *btnRunUntil;
+                QSpinBox *spnBreakAt;
+
+        QGroupBox *grpSimulationOutputControls;
+            QGridLayout *grdloutSimulationOutput;
+                QLabel *lblMemAccessInstances;
+                QLCDNumber *lcdMemAccessInstances;\
+                QLabel *lblCompMissRatio;
+                QLCDNumber *lcdCompMissRatio;
+                QLabel *lblHitRatio;
+                QLCDNumber *lcdHitRatio;
+                QLabel *lblMissRatio;
+                QLCDNumber *lcdMissRatio;
+
+        QPushButton *quit;
+};
+
+#endif // MAINWINDOW_H
